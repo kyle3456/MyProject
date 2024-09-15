6f867ed39931d901d099962413dcce7eb480425a
kyle3456 <96670192+kyle3456@users.noreply.github.com>
2024-09-14 17:12:28 -0700

    account

--- myproject/lib/screens/homescreen.dart
+++ myproject/lib/screens/homescreen.dart
@@ -1,8 +1,12 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:myproject/size_config.dart';
 import 'package:myproject/components/person_card.dart';
 import 'package:myproject/components/NavBar.dart';
 import 'package:myproject/shared/singleton.dart';
+// import 'package:google_maps/google_maps.dart' as gmaps;
 
 // class Person {
 //   final String name;
@@ -40,6 +44,14 @@ class _HomeScreenState extends State<HomeScreen> {
   //       imagePath: 'assets/Profile Picture.png'),
   // ];
 
+    final Completer<GoogleMapController> _controller =
+      Completer<GoogleMapController>();
+
+    static const CameraPosition _kGooglePlex = CameraPosition(
+      target: LatLng(37.42796133580664, -122.085749655962),
+      zoom: 14.4746,
+    );
+
   @override
   Widget build(BuildContext context) {
     Singleton singleton = Singleton();
@@ -67,30 +79,41 @@ class _HomeScreenState extends State<HomeScreen> {
                           },
                         )),
                   ),
-                  Container(
-                      width: SizeConfig.blockSizeHorizontal! * 80,
-                      height: SizeConfig.blockSizeVertical! * 55,
-                      color: const Color.fromARGB(255, 76, 175, 79),
-                      child: Stack(
-                        children: [
-                          Positioned(
-                              top: 10,
-                              left: 0,
-                              right: 0,
-                              child: Container(
-                                width: SizeConfig.blockSizeHorizontal! * 10,
-                                height: SizeConfig.blockSizeHorizontal! * 10,
-                                decoration: const BoxDecoration(
-                                    color: Colors.white,
-                                    shape: BoxShape.circle),
-                                child: Padding(
-                                  padding: const EdgeInsets.all(2.0),
-                                  child:
-                                      Image.asset('assets/Profile Picture.png'),
-                                ),
-                              ))
-                        ],
-                      ))
+                  SizedBox(
+                    width: SizeConfig.blockSizeHorizontal! * 80,
+                    height: SizeConfig.blockSizeVertical! * 55,
+                    // child: GoogleMap(
+                    //   mapType: MapType.satellite,
+                    //   initialCameraPosition: _kGooglePlex,
+                    //   onMapCreated: (GoogleMapController controller) {
+                    //     _controller.complete(controller);
+                    //   },
+                    // ),
+                  ),
+                  // Container(
+                  //     width: SizeConfig.blockSizeHorizontal! * 80,
+                  //     height: SizeConfig.blockSizeVertical! * 55,
+                  //     color: const Color.fromARGB(255, 76, 175, 79),
+                  //     child: Stack(
+                  //       children: [
+                  //         Positioned(
+                  //             top: 10,
+                  //             left: 0,
+                  //             right: 0,
+                  //             child: Container(
+                  //               width: SizeConfig.blockSizeHorizontal! * 10,
+                  //               height: SizeConfig.blockSizeHorizontal! * 10,
+                  //               decoration: const BoxDecoration(
+                  //                   color: Colors.white,
+                  //                   shape: BoxShape.circle),
+                  //               child: Padding(
+                  //                 padding: const EdgeInsets.all(2.0),
+                  //                 child:
+                  //                     Image.asset('assets/Profile Picture.png'),
+                  //               ),
+                  //             ))
+                  //       ],
+                  //     ))
                 ],
               ),
             ),
